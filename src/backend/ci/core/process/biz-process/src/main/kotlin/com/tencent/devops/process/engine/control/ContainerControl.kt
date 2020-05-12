44c5eca01620ed0bf921219cc0ea5e9fec9a886d
fitzcao <fitzcao@tencent.com>
2020-05-12 14:37:12 +0800

    feature:流水线TASK执行前支持自动暂停, container逻辑调整

--- src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/control/ContainerControl.kt
+++ src/backend/ci/core/process/biz-process/src/main/kotlin/com/tencent/devops/process/engine/control/ContainerControl.kt
@@ -91,7 +91,7 @@ class ContainerControl @Autowired constructor(
         }
 
         // 当build的状态是结束的时候，直接返回
-        if (BuildStatus.isFinish(container.status)) {
+        if (BuildStatus.isFinish(container.status) || container.status.equals(BuildStatus.PAUSE)) {
             return
         }
 
